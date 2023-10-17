76adec4582ec643cc12aeadce95beda3935a561a
14Richa <onlinericha19@gmail.com>
2023-10-18 01:28:39 +0530

    added another function

--- Week2/hashtable.py
+++ Week2/hashtable.py
@@ -18,12 +18,34 @@ def matrix_multiplication(H, lst):
     for row in H:
         result.append(dot_product(row, lst))
     return result
-    # Generate a random m \times n matrix
-    # see the comment next to matrix_multiplication for how your matrix must be returned.
 
 H = [[0,1,0,1],[1,0,0,0],[1,0,1,1]]
 lst = [1, 1, 1, 0]
 
 result = matrix_multiplication(H, lst)
-# Print 
 print(result)
+
+
+# Generate a random m times n matrix
+# see the comment next to matrix_multiplication for how your matrix must be returned.
+
+def return_random_hash_function(m, n):
+    # return a random hash function wherein each entry is chosen as 1 with probability >= 1/2 and 0 with probability < 1/2
+    # your code here
+    matrix = []
+    for i in range(m):
+        row = []
+        for j in range(n):
+            random_num = random()
+            print(random_num)
+            if random_num >= 0.5:
+                val = 1
+            else: 
+                val = 0
+            row.append(val)
+        matrix.append(row)
+    return matrix
+
+matrix1 = return_random_hash_function(3, 4)
+# Print 
+print(matrix1)
