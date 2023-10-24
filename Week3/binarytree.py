24b435a83ccae2e2313866b1920dcc6ba3ce6cc4
14Richa <onlinericha19@gmail.com>
2023-10-24 17:45:37 +0530

    added week3 folder

---
+++ Week3/binarytree.py
@@ -0,0 +1,55 @@
+# Week 3 - In class activity
+
+class Node: 
+    # Implement a node of the binary search tree.
+    # Constructor for a node with key and a given parent
+    # parent can be None for a root node.
+    def __init__(self, key, parent = None): 
+        self.key = key
+        self.parent = parent 
+        self.left = None # We will set left and right child to None
+        self.right = None
+        # Make sure that the parent's left/right pointer
+        # will point to the newly created node.
+        if parent != None:
+            if key < parent.key:
+                assert(parent.left == None), 'parent already has a left child -- unable to create node'
+                parent.left = self
+            else: 
+                assert key > parent.key, 'key is same as parent.key. We do not allow duplicate keys in a BST since it breaks some of the algorithms.'
+                assert(parent.right == None ), 'parent already has a right child -- unable to create node'
+                parent.right = self
+        
+    # Utility function that keeps traversing left until it finds 
+    # the leftmost descendant
+    def get_leftmost_descendant(self):
+        if self.left != None:
+            return self.left.get_leftmost_descendant()
+        else:
+            return self
+    
+    # TODO: Complete the search algorithm below
+    # You can call search recursively on left or right child
+    # as appropriate.
+    # If search succeeds: return a tuple True and the node in the tree
+    # with the key we are searching for.
+    # Also note that if the search fails to find the key 
+    # you should return a tuple False and the node which would
+    # be the parent if we were to insert the key subsequently.
+    def search(self, key):
+        if self.key == key: 
+            return (True, self)
+        # your code here
+        if key < self.key:
+            if self.left:
+              return self.left.search(key)
+            else:
+              return (False, self)
+
+        if key > self.key:
+            if self.right:
+              return self.right.search(key)
+            else:
+              return (False, self)
+    
+    
